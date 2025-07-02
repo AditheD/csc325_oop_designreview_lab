1f63fbc5fbc56e19417ae2e1165fe3bb139f4f73
AditheD <adithedas20@gmail.com>
2025-07-02 00:32:45 -0400

    Initial commit

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -11,34 +11,83 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class MainClass {
 
  public static void main(String[] args) {
-		// ToDo 5: Fix the error
+	 // ToDo 5: Fix the error
+	 // ToDo 6: Fix the constructor of the Student class
+	 // Todo 7: Create two classes for Freshman and Senior
+	 // ToDo 8: The senior class should have a minimum of 85 credits
+	 // ToDo 9: Add a toString method for the Student class
+	 // ToDo 10: Add a toString method for the Freshman class
+	 // ToDo 11: Add a toString method for the Senior class
+	 // ToDo 12: Set the gpa of the student using the scanner and user
+	 //  input and then print the output.
+	 // ToDo 13: add comments and explain your code
+/**
+ * Constructing a Freshman with specified name, age, and credits.
+ */
+	 class Freshman extends Student {
+		 public Freshman(String name, short age, int credits) {
+			 super(name, age, credits);
 
-		// ToDo 6: Fix the constructor of the Student class
+		 }
+		 @Override
+		 public String toString() {
+			 return "Freshman{" +
+					 "name=" + getName() +
+					 ", age=" + getAge() +
+					 ", credits=" + getCredits() +
+					 ", GPA=" + getGpa() +
+					 '}';
 
-                // Todo 7: Create two classes for Freshman and Senior 
+		 }
+	 }
 
-                // ToDo 8: The senior class should have a minimum of 85 credits  
+	 /**
+	  * A Senior student inherits from the Student class.
+	  * A Senior must have at least 85 credits to be eligible.
+	  * */
 
-		// ToDo 9: Add a toString method for the Student class
-		// ToDo 10: Add a toString method for the Freshman class
+	 class Senior extends Student {
+		 public Senior(String name, short age, int credits) {
+			 super(name, age, credits);
+			 if (credits < 85) {
+				 throw new IllegalArgumentException("Senior must have at least 85 credits.");
+			 }
+		 }
 
-		Student std1= new Student("James", 20);
-                // ToDo 11: Add a toString method for the Senior class
+		 @Override
+		 public String toString() {
+			 return "Senior{" +
+					 "name=" + getName() +
+					 ", age=" + getAge() +
+					 ", credits=" + getCredits() +
+					 ", GPA=" + getGpa() +
+					 '}';
+		 }
+	 }
+/**
+ * Create student objects
+ */
+	 Freshman std1 = new Freshman("James", (short) 20, 12);
 
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
+	 Senior std2 = new Senior("John", (short) 30, 90);
+/**
+ * Create scanner for GPA
+ */
+	 java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-                Senior std2 = new Student("John", 30, 90);
+	 System.out.print("Enter GPA for Freshman James: ");
+	 std1.setGpa(scanner.nextDouble());
 
-		// ToDo 12: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
+	 System.out.print("Enter GPA for Senior John: ");
+	 std2.setGpa(scanner.nextDouble());
+/**
+ * Output results
+ */
+	 System.out.println(std1);
 
-		System.out.println(std1);
-
-                System.out.println(std2);
-
-		// ToDo 13: add comments and explain your code
-
-	}
+	 System.out.println(std2);
+ }
 
 }
+
 
