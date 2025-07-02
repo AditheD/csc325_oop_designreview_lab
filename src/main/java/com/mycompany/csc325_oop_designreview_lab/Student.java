92cd36fe61311103065e4b5401f58b4202d6346a
AditheD <adithedas20@gmail.com>
2025-07-02 00:31:33 -0400

    Initial commit Make student class a child of Human Add a field for GPA and create a setter and a getter

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,12 +8,61 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
+public class Student extends Human{
 	// ToDo 1: Make this class a child of Human
 
 	// ToDo 2: Fix the resulting errors
 
 	// ToDo 3: Add a field for GPA and create a setter and a getter
-	
-	// ToDo 4: Add comments to your code
+
+    // ToDo 4: Add comments to your code
+    /**
+     *  store the student's GPA
+     */
+    private double gpa;
+    /**
+     *store the student's address
+     */
+    private String address;
+    /**
+     * store the student's credits
+     */
+    private int credits;
+
+    /**
+     * Initializes name, age, and credits using superclass constructor
+     */
+    public Student(String name, short age, int credits) {
+        super(name, age);
+        this.credits = credits;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Getter for GPA
+    public double getGpa() {
+        return gpa;
+    }
+
+    // Setter for GPA
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
 }
